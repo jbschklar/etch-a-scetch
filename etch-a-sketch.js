6307a77aace58a12f2116bdc314e0e508a7235db
jbschklar <jbschklar@gmail.com>
2022-05-17 16:07:19 -0400

    added buttons and contained app in start button eventListener

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,35 +1,59 @@
 "use strict";
 
 const gridContainer = document.querySelector(".sketch-pad");
-const gridNum = 50;
-const gridCountTotal = Math.pow(gridNum, 2);
+const btnStart = document.querySelector(".prompt");
+const btnReset = document.querySelector(".reset");
 const gridSqSize = 900;
-
-for (let i = 0; i < gridCountTotal; i++) {
-	const gridItem = document.createElement("div");
-	gridItem.classList.add("grid-item");
-	gridContainer.appendChild(gridItem);
-}
-
 const gridItemSize = 8;
-const gridWidth = gridNum * gridItemSize;
-console.log(gridWidth);
-const gridItems = document.querySelectorAll(".grid-item");
+let gridNum;
 
-gridContainer.style.cssText = `
-   
+// Prompts user for pixel number
+const getGridNum = function () {
+	const num = prompt("Choose the pixel size from 10 to 100");
+	return +num;
+};
+
+// Starts application when the start button is clicked
+btnStart.addEventListener("click", function () {
+	// Resets the grid back to blank canvas
+	const reset = function () {
+		gridItems.forEach((grid) => {
+			grid.style.backgroundColor = "white";
+		});
+	};
+
+	gridNum = getGridNum();
+
+	if (!gridNum || gridNum < 10 || gridNum > 100) {
+		alert("Invalid Number");
+		return;
+	}
+
+	const gridCountTotal = Math.pow(gridNum, 2);
+
+	// Creates 'pixels' using user's selected number to adjust grid item size relative to the container
+	for (let i = 0; i < gridCountTotal; i++) {
+		const gridItem = document.createElement("div");
+		gridItem.classList.add("grid-item");
+		gridContainer.appendChild(gridItem);
+	}
+
+	const gridItems = document.querySelectorAll(".grid-item");
+
+	// Allows creation of the gridcontainer based on prompted user input value a.k.a gridNum
+	gridContainer.style.cssText = `
     grid-template-columns: repeat(${gridNum}, 1fr);
 	grid-template-rows: repeat(${gridNum}, 1fr);
-    width: ${gridSqSize}px;
-    height: ${gridSqSize}px;
-    
-`;
+    `;
 
-console.log(gridItems.length);
+	// Creates etch-a-sketch effect
+	gridItems.forEach((grid) => {
+		grid.addEventListener("mousemove", function () {
+			grid.style.backgroundColor = "black";
+		});
+	});
 
-gridItems.forEach((grid) => {
-	grid.addEventListener("mouseover", function () {
-		// grid.style.backgroundColor = "black";
-		grid.classList.add("active");
+	btnReset.addEventListener("click", function () {
+		reset();
 	});
 });
